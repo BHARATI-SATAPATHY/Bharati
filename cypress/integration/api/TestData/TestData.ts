a8bfa4c7b1dbacb87cb0e6b5e30ea6a5881fa850
BHARATISATAPATHY <bharati.satapathy@ness.com>
2021-03-15 17:18:06 +0530

    Commits1

---
+++ cypress/integration/api/TestData/TestData.ts
@@ -0,0 +1,28 @@
+export class ResponseData {
+
+    //response data
+    getStatus() : number
+    {
+        let status : number =200;
+        return status;
+    }
+
+    getProperty() : string
+    {
+        let page : string = '\'page\', 2'
+        return page;
+    }
+
+    getLength() : number
+    {
+        let length : number = 6
+        return length;
+    }
+    getEmail() : string
+    {
+        let email : string = "michael.lawson@reqres.in"
+        return email;
+    }
+
+    
+}
